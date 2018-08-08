fe4b7dd41b9336b0adc1ce163bc7e4c07a486408
DevoRoberts <devorob26@gmail.com>
2018-08-08 19:35:23 -0400

    Initail commit C#

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Using directive
+using System;
+
+namespace Basics.FitnessFrog {
+
+    
+    class Program {
+
+        static void Main() {
+            //Prompt the user for minutes exercised
+            Console.Write("Enter how many minutes you exercised: ");
+                //returns what the user typed
+            string entry = Console.ReadLine();
+            //Add minute exercised to total
+
+            //Display total minutes exercised to the scren
+            Console.WriteLine($"You've exercised {entry} minutes");
+            //Repeat until user quits
+        }
+    }
+}
